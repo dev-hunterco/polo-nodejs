7758d3f0f96b254a9afa6b31ac79b94072c5f503
Flavio Cordova <flavio.cordova@gmail.com>
2020-05-29 14:10:20 -0300

    publish @next versions

---
+++ version-updater.js
@@ -0,0 +1,14 @@
+const currentVersion = process.env.TRAVIS_TAG || '@next'
+const fs = require('fs');
+console.log('Updating package.json to version:', currentVersion)
+
+const currentContent = require('./package.json')
+
+currentContent.version = currentVersion
+
+fs.writeFile("./new-package.json", JSON.stringify(currentContent, null, 2), function(err) {
+  if (err) {
+    console.log(`Error saving file ${file}:`, err);
+    process.exit(1)
+  }
+});
